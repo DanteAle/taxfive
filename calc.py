f8fe172d4ac608d01f3d2a123e84b86d88f7116c
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-09-12 09:40:55 -0500

    feat (draft): First steps

---
+++ calc.py
@@ -0,0 +1,20 @@
+from main import app
+
+RM_1 = 0
+M = 0
+ext = 0
+RBA_1 = 0
+UIT = 0
+M_R = 0
+IAP_1 = 0
+suma = 0
+RNA_1 = 0
+R_M_1 = 0
+TRM = 0
+MA = 0
+MIA = 0
+RMA = 0
+var = 0
+RAM_1 = 0
+app(RM_1, M, ext, RBA_1, UIT, M_R, IAP_1, suma,
+    RNA_1, R_M_1, TRM, MA, MIA, RMA, var, RAM_1)
